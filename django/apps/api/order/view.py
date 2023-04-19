dc01c655a04189ee2da58649286602d5546defe7
Roman Myasnikov <rmyasnikov@sdvor.com>
2023-04-19 23:52:41 +0500

    django api example

--- django/apps/api/order/view.py
+++ django/apps/api/order/view.py
@@ -2,4 +2,9 @@ from apps import api
 
 
 class OrderView(api.order.v1.view.OrderView):
+    """Общий view для всех версий
+
+    View всех версий подключаются сюда через множественное наследование.
+    Этот view указывается в urls.py
+    """
     pass
